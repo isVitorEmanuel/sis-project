6fe5dfa19f2222b804f8574460092c2887b1ad93
Vitor Emanuel <vitoralencar2706@gmail.com>
2025-01-12 08:32:00 -0300

    refactor - remove warnings

--- src/main/java/com/lp2/sisproject/controller/ProductsInfoController.java
+++ src/main/java/com/lp2/sisproject/controller/ProductsInfoController.java
@@ -1,23 +1,4 @@
 package com.lp2.sisproject.controller;
 
-import com.lp2.sisproject.util.ManipulatingWindow;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 public class ProductsInfoController {
-    private final ManipulatingWindow window = new ManipulatingWindow();
-
-    @FXML
-    public void toProducts(ActionEvent event) {
-        this.window.toProductsView(event);
-    }
 }
