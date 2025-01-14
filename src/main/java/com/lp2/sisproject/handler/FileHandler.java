6d75d5c3c9f499339e82ccf581ec0de39a74e98e
Vitor Emanuel <vitoralencar2706@gmail.com>
2025-01-14 19:12:44 -0300

    connect ok

--- src/main/java/com/lp2/sisproject/handler/FileHandler.java
+++ src/main/java/com/lp2/sisproject/handler/FileHandler.java
@@ -4,49 +4,53 @@ import com.lp2.sisproject.model.Manufacturer;
 import com.lp2.sisproject.model.Product;
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class FileHandler {
-    public static void write(ArrayList<Product> products, ArrayList<Manufacturer> manufacturers){
-        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Products.txt"))){
+    public static void write(ArrayList<Product> products, ArrayList<Manufacturer> manufacturers) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src/main/resources/data/products.bin"))) {
             oos.writeObject(products);
             System.out.println("Products saved to file");
-        }catch(IOException e){
-            //TODO: Implementar exceptions personalizadas?
-            System.err.println(e);
+        } catch (IOException e) {
+            // TODO: Handler exception.
+            System.err.println(e.getMessage());
         }
-        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Manufacturers.txt"))){
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src/main/resources/data/manufacturers.bin"))) {
             oos.writeObject(manufacturers);
             System.out.println("Manufacturers saved to file");
-        }catch(IOException e){
-            //TODO: Implementar exceptions personalizadas?
-            System.err.println(e);
+        } catch (IOException e) {
+            // TODO: Handler exception.
+            System.err.println(e.getMessage());
         }
     }
-    public static void read(ArrayList<Product> products, ArrayList<Manufacturer> manufacturers){
 
-        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Products.txt"))){
-            //TODO: Ajeitar esse warning, ou nao.
-            ArrayList<Product> productsFromFile = (ArrayList<Product>) ois.readObject();
-
-            products.addAll(productsFromFile);
-        System.out.println("Products read from file");
-        }catch(IOException | ClassNotFoundException e){
+    @SuppressWarnings("unchecked")
+    public static void read(ArrayList<Product> products, ArrayList<Manufacturer> manufacturers) {
+        try (InputStream productStream = FileHandler.class.getClassLoader().getResourceAsStream("data/products.bin")) {
+            if (productStream != null) {
+                ObjectInputStream ois = new ObjectInputStream(productStream);
+                ArrayList<Product> productsFromFile = (ArrayList<Product>) ois.readObject();
+                products.addAll(productsFromFile);
+                System.out.println("Products read from file");
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            // TODO: Handler exception.
             System.err.println("Error reading the products file");
-            //TODO: Implementar exceptions personalizadas?
         }
-        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Manufacturers.txt"))){
 
-            ArrayList<Manufacturer> manufacturersFromFile = (ArrayList<Manufacturer>) ois.readObject();
-
-            manufacturers.addAll(manufacturersFromFile);
-
-        System.out.println("Manufacturers read from file");
-        }catch(IOException | ClassNotFoundException e){
+        try (InputStream manufacturerStream = FileHandler.class.getClassLoader().getResourceAsStream("data/manufacturers.bin")) {
+            if (manufacturerStream != null) {
+                ObjectInputStream ois = new ObjectInputStream(manufacturerStream);
+                ArrayList<Manufacturer> manufacturersFromFile = (ArrayList<Manufacturer>) ois.readObject();
+                manufacturers.addAll(manufacturersFromFile);
+                System.out.println("Manufacturers read from file");
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            // TODO: Handler exception.
             System.err.println("Error reading the manufacturers file");
         }
     }
+}
 
-    }
 
