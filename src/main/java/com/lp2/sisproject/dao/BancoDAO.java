51e30ad9ba1b1991927beb4d05e8c2253f484deb
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2025-01-14 10:04:18 -0300

    Update DAO and FileHandler
    Updates on handling manufacturers

--- src/main/java/com/lp2/sisproject/dao/BancoDAO.java
+++ src/main/java/com/lp2/sisproject/dao/BancoDAO.java
@@ -1,11 +1,15 @@
 package com.lp2.sisproject.dao;
 
+import com.lp2.sisproject.model.Manufacturer;
 import com.lp2.sisproject.model.Product;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class BancoDAO {
     private ArrayList<Product> products;
+
+    private ArrayList<Manufacturer> manufacturers;
 
     private static BancoDAO bancoDAO;
 
@@ -20,5 +24,8 @@ public class BancoDAO {
     }
     public ArrayList<Product> getProdutos() {
         return products;
+    }
+    public ArrayList<Manufacturer> getManufacturers() {
+        return manufacturers;
     }
 }
--- src/main/java/com/lp2/sisproject/handler/FileHandler.java
+++ src/main/java/com/lp2/sisproject/handler/FileHandler.java
@@ -1,12 +1,14 @@
 package com.lp2.sisproject.handler;
 
+import com.lp2.sisproject.model.Manufacturer;
 import com.lp2.sisproject.model.Product;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class FileHandler {
-    public static void write(ArrayList<Product> products){
+    public static void write(ArrayList<Product> products, ArrayList<Manufacturer> manufacturers){
         try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Products.txt"))){
             oos.writeObject(products);
             System.out.println("Products saved to file");
@@ -14,19 +16,29 @@ public class FileHandler {
             //TODO: Implementar exceptions personalizadas?
             System.err.println(e);
         }
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Manufacturers.txt"))){
+            oos.writeObject(manufacturers);
+            System.out.println("Manufacturers saved to file");
+        }catch(IOException e){
+            //TODO: Implementar exceptions personalizadas?
+            System.err.println(e);
+        }
     }
-    public static void read(ArrayList<Product> products){
+    public static void read(ArrayList<Product> products, ArrayList<Manufacturer> manufacturers){
 
         try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Products.txt"))){
-            //TODO: Ajeitar esse warning, se pá...
-            ArrayList<Product> produtos = (ArrayList<Product>) ois.readObject();
+            //TODO: Ajeitar esse warning, ou nao.
+            ArrayList<Product> productsFromFile = (ArrayList<Product>) ois.readObject();
+            ArrayList<Manufacturer> manufacturersFromFile = (ArrayList<Manufacturer>) ois.readObject();
 
-            products.addAll(produtos);
+            manufacturers.addAll(manufacturersFromFile);
+            products.addAll(productsFromFile);
         }catch(IOException | ClassNotFoundException e){
             System.err.println("Error reading the products file");
             //TODO: Implementar exceptions personalizadas?
         }
         System.out.println("Products read from file");
+        System.out.println("Manufacturers read from file");
     }
 
     }
